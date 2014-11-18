9cec8939070f50529344b597f8ecfef313ec7547
Dmytro Bignyak <dmytro.bignyak@onapp.com>
2014-11-18 19:34:08 +0200

    add accessibility to fetch internal disks attributes

--- lib/ruby_vcloud_sdk/internal_disk.rb
+++ lib/ruby_vcloud_sdk/internal_disk.rb
@@ -1,13 +1,26 @@
 module VCloudSdk
 
   class InternalDisk
-    attr_reader :name, :capacity, :bus_type, :bus_sub_type
+    attr_reader :name, :capacity, :bus_type, :bus_sub_type, :storage_profile_link
 
     def initialize(entity_xml)
       @name = entity_xml.element_name
       @capacity = entity_xml.host_resource.attribute("capacity").to_s.to_i
       @bus_type = entity_xml.host_resource.attribute("busType").to_s
       @bus_sub_type = entity_xml.host_resource.attribute("busSubType").to_s
+      @storage_profile_link = entity_xml.host_resource.attribute("storageProfileHref").value
+    end
+
+    def to_hash
+      {
+        name: name,
+        capacity: capacity,
+        bus_sub_type: bus_sub_type
+      }
+    end
+
+    def storage_profile_id
+      URI.parse(storage_profile_link).path.split('/')[-1]
     end
   end
 end
